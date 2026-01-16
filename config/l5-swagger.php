1b627e82d12408d8e26822ff1ce69a0437e7c418
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-01-16 22:05:40 +0500

    sd

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -1,7 +1,8 @@
-<?php
+<!-- <?php
 
 return [
     'default' => 'default',
+
     'documentations' => [
         'default' => [
             'api' => [
@@ -9,16 +10,16 @@ return [
             ],
 
             'routes' => [
-                'api' => 'api/documentation',
+                'api' => 'documentation',
             ],
 
             'paths' => [
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+                'use_absolute_path' => true,
                 'docs_json' => 'api-docs.json',
                 'docs_yaml' => 'api-docs.yaml',
-                'format_to_use_for_docs' => env('L5_FORMAT_TO_USE_FOR_DOCS', 'json'),
+                'format_to_use_for_docs' => 'json',
                 'annotations' => [
-                    base_path('app/Http/Controllers'),
+                    base_path('app'),
                 ],
             ],
         ],
@@ -27,7 +28,7 @@ return [
     'defaults' => [
         'routes' => [
             'docs' => 'docs',
-            'oauth2_callback' => 'api/oauth2-callback',
+            'oauth2_callback' => 'oauth2-callback',
             'middleware' => [
                 'api' => [],
                 'asset' => [],
@@ -40,19 +41,28 @@ return [
         'paths' => [
             'docs' => storage_path('api-docs'),
             'views' => base_path('resources/views/vendor/l5-swagger'),
-            'base' => env('L5_SWAGGER_BASE_PATH', '/api'),
-            'swagger_ui_assets_path' => env('L5_SWAGGER_UI_ASSETS_PATH', 'vendor/swagger-api/swagger-ui/dist/'),
+
+            // ✅ API BASE COMPLETELY REMOVED
+            'base' => '',
+
+            'swagger_ui_assets_path' => 'vendor/swagger-api/swagger-ui/dist/',
             'excludes' => [],
         ],
 
+        // ✅ NO /api ANYWHERE
+        'servers' => [
+            [
+                'url' => 'http://localhost:8000',
+                'description' => 'Local Server',
+            ],
+            [
+                'url' => 'https://pickpackgo.in-sourceit.com',
+                'description' => 'Production Server',
+            ],
+        ],
+
         'scanOptions' => [
-            'default_processors_configuration' => [],
-            'analyser' => null,
-            'analysis' => null,
-            'processors' => [],
-            'pattern' => null,
-            'exclude' => [],
-            'open_api_spec_version' => env('L5_SWAGGER_OPEN_API_SPEC_VERSION', \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION),
+            'open_api_spec_version' => \L5Swagger\Generator::OPEN_API_DEFAULT_SPEC_VERSION,
         ],
 
         'securityDefinitions' => [
@@ -70,24 +80,11 @@ return [
             ],
         ],
 
-        'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', false),
-        'generate_yaml_copy' => env('L5_SWAGGER_GENERATE_YAML_COPY', false),
+        'generate_always' => false,
+        'generate_yaml_copy' => false,
         'proxy' => false,
-        'additional_config_url' => null,
-        'operations_sort' => env('L5_SWAGGER_OPERATIONS_SORT', null),
-        'validator_url' => null,
-        'ui' => [
-            'display' => [
-                'dark_mode' => env('L5_SWAGGER_UI_DARK_MODE', false),
-                'doc_expansion' => env('L5_SWAGGER_UI_DOC_EXPANSION', 'none'),
-                'filter' => env('L5_SWAGGER_UI_FILTERS', true),
-            ],
-            'authorization' => [
-                'persist_authorization' => env('L5_SWAGGER_UI_PERSIST_AUTHORIZATION', false),
-            ],
-        ],
-        'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://localhost:8000'),
-        ],
+
+        // ❌ HOST GUESSING OFF
+        'constants' => [],
     ],
 ];
